400880fc731f72c0ce27c581afe8b6c6513ddb30
xPhantomx <62573048+Phantom93x@users.noreply.github.com>
2021-05-06 06:41:33 +0300

    react app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import Cardlist from './Cardlist.js';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import {robots} from './robots.js';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Cardlist robots={robots}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
